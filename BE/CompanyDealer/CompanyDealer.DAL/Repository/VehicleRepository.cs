285acf2f7da11819ff494d8d38ca45cb39e2a86e
Rias070 <minhndkse182351@fpt.edu.vn>
2025-10-16 23:37:10 +0700

    Create VehicleRepository.cs
    Vehicle Repo

---
+++ BE/CompanyDealer/CompanyDealer.DAL/Repository/VehicleRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyDealer.DAL.Repositories
+{
+    public class VehicleRepository
+    {
+        private readonly DbContext _context;
+
+        public VehicleRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        // Create
+        public async Task<Vehicle> CreateAsync(Vehicle vehicle)
+        {
+            _context.Set<Vehicle>().Add(vehicle);
+            await _context.SaveChangesAsync();
+            return vehicle;
+        }
+
+        // Read (Get by Id)
+        public async Task<Vehicle?> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<Vehicle>()
+                .Include(v => v.Inventory)
+                .Include(v => v.Category)
+                .Include(v => v.TestDriveRecords)
+                .FirstOrDefaultAsync(v => v.Id == id);
+        }
+
+        // Read (Get all)
+        public async Task<List<Vehicle>> GetAllAsync()
+        {
+            return await _context.Set<Vehicle>()
+                .Include(v => v.Inventory)
+                .Include(v => v.Category)
+                .Include(v => v.TestDriveRecords)
+                .ToListAsync();
+        }
+
+        // Update
+        public async Task<Vehicle?> UpdateAsync(Vehicle vehicle)
+        {
+            var existingVehicle = await _context.Set<Vehicle>().FindAsync(vehicle.Id);
+            if (existingVehicle == null)
+            {
+                return null;
+            }
+
+            _context.Entry(existingVehicle).CurrentValues.SetValues(vehicle);
+            await _context.SaveChangesAsync();
+            return existingVehicle;
+        }
+
+        // Delete
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var vehicle = await _context.Set<Vehicle>().FindAsync(id);
+            if (vehicle == null)
+            {
+                return false;
+            }
+
+            _context.Set<Vehicle>().Remove(vehicle);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}

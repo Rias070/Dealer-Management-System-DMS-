3807510b5c20af703fb0fb9d922c88ef043c6703
Duy Phan <144097056+whisama613@users.noreply.github.com>
2025-10-24 21:03:05 +0700

    Roles API

---
+++ BE/CompanyDealer/CompanyDealer.BLL/DTOs/RoleDTOs/CreateRoleDto.cs
@@ -0,0 +1,7 @@
+namespace CompanyDealer.BLL.DTOs.RoleDTOs
+{
+    public class CreateRoleDto
+    {
+        public string RoleName { get; set; } = null!;
+    }
+}
--- BE/CompanyDealer/CompanyDealer/Program.cs
+++ BE/CompanyDealer/CompanyDealer/Program.cs
@@ -130,6 +130,7 @@ builder.Services.AddScoped<RestockRequestService>();
 // Dealer services
 builder.Services.AddScoped<CompanyDealer.DAL.Repository.DealerRepo.IDealerRepository, CompanyDealer.DAL.Repository.DealerRepo.DealerRepository>();
 builder.Services.AddScoped<DealerService>();
+builder.Services.AddScoped<CompanyDealer.BLL.Services.IRoleService, CompanyDealer.BLL.Services.RoleService>();
 
 
 // Register repository interface
